e54d1ae0f3847e44b3a11d2f6ed4c6b713923144
SchulzRob <Schulzro@students.uni-marburg.de>
2023-03-15 14:42:53 +0100

    added Groundcheck

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -20,7 +20,10 @@ public class PlayerController : MonoBehaviour
     public Transform holder;
     TextMeshProUGUI healthText;
     TextMeshProUGUI scoreText;
-
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    CircleCollider2D Ccollider2D;
+    BoxCollider2D Bcollider2D;
     Transform myTransform;
     SpriteRenderer mySprite;
     Rigidbody2D rb;
@@ -39,6 +42,8 @@ public class PlayerController : MonoBehaviour
         mySprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+         Ccollider2D = GetComponent<CircleCollider2D>();
+        Bcollider2D = GetComponent<BoxCollider2D>();
         healthText = holder.Find("TxtHealth").GetComponent<TextMeshProUGUI>();
         scoreText = holder.Find("TxtScore").GetComponent<TextMeshProUGUI>();
         scoreText.text = "Score :" + score; //"Score : 0";
@@ -73,32 +78,30 @@ public class PlayerController : MonoBehaviour
             mySprite.flipX = true;
 
 
-        if (Input.GetButtonDown("Jump") && !isJump)
+        if (Input.GetButtonDown("Jump") && isGrounded() )
         {
             rb.velocity = new Vector2(0, Jump);
-            isJump = true;
+            
+        }
+        if (Mathf.Abs(rb.velocity.y) < 0.01f){
+            
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
         }
 
-        if (Mathf.Abs(rb.velocity.y) < 0.01f)
-            isJump = false;
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-
         
-        if (Mathf.Abs(rb.velocity.y) < 0.01f)
-            isJump = false;
-
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+       
         
     }
     private void FixedUpdate()
     {
+        
         rb.velocity = new Vector2(Speed * Input.GetAxis("Horizontal"), rb.velocity.y);
         //Debug.Log(Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isJump == false)
+        if(Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(0, Jump);
-            isJump = true;
+            
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -106,7 +109,7 @@ public class PlayerController : MonoBehaviour
         if (collision.CompareTag("Enamy"))
         {
 
-            if (isJump && rb.velocity.y < 0)
+         if (isGrounded() == false && rb.velocity.y < 0)
             {
                 Destroy(collision.gameObject);
 
@@ -163,6 +166,22 @@ public class PlayerController : MonoBehaviour
                 healthText.text = PlayerHealth + "/" + maxHealth;
             }
         }
+    }
+    private bool isGrounded()
+    {
+       
+        RaycastHit2D raycastHit = Physics2D.Raycast(Ccollider2D.bounds.center,Vector2.down,Ccollider2D.bounds.extents.y+ .5f,groundLayer);
+        Color raycolor;
+        if(raycastHit.collider != null) {
+            raycolor = Color.green;
+        }
+        else{
+            raycolor = Color.red;
+        }
+
+        Debug.DrawRay(Ccollider2D.bounds.center, Vector2.down * (Ccollider2D.bounds.extents.y + 1.5f), Color.red);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
     }
 
 }
